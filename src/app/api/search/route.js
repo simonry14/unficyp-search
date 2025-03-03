7843e9973651cc7a17eb31810a4b185d07b230ba
Simon Peter Miyingo <simonpetermiyingo@gmail.com>
2025-03-03 11:51:21 -0500

    updated dummy data

--- src/app/api/search/route.js
+++ src/app/api/search/route.js
@@ -7,17 +7,24 @@ export async function GET(request) {
   // This is mock data - in a real application, you would fetch results from your database or external API
   const mockResults = [
     {
-      title: `TD/B/CN.1/4 - PREVIOUS WORK IN UNCTAD ON TUNGSTEN AND IRON ORE : NOTE / BY THE UNCTAD SECRETARIAT`,
-      description: `Distr. GENERALE TD/B/CN.1/4 10 août 1992 FRANCAIS Original : ANGLAIS CONSEIL DU COMMERCE ET DU DEVELOPPEMENT Commission permanente des produits de base Première session Genève, 19 octobre 1992 Point 4 de l'ordre du jour provisoire ORGANES SUBSIDIAIRES DE LA COMMISSION PERMANENTE (PARAGRAPHE 7 DU MANDAT DE LA COMMISSION) Travaux précédents de la CNUCED sur le tungstène et le minerai de fer`,
+      title: `S/AGENDA/7229 - AGENDA - UNFICYP`,
+      description: `United Nations S/Agenda/7229 Security Council Distr.: General 30 July 2014 Original: English 14-58484 (E) 300714 *1458484* Provisional agenda for the 7229th meeting of the Security Council To be held on Wednesday, 30 July 2014, at 10 a.m. 1. Adoption of the agenda. 2. The situation in Cyprus Report of the Secretary-General on the United Nat`,
       dataSource: "ODS",
-      url: "https://documents.un.org/symbol-explorer?s=TD/B/CN.1/4_4285785"
+      url: "https://documents.un.org/symbol-explorer?s=S ... 7229_4654109"
     },
     {
-      title: `TD/B/WP/330 - EVALUATION OF UNCTAD ACTIVITIES: OVERVIEW`,
-      description: `GE.24-13785 (E) Trade and Development Board Working Party on the Programme Plan and Programme Performance Eighty-eighth session Geneva, 7–11 October 2024 Item 4 (a) of the provisional agenda Evaluation of UNCTAD activities: Overview Report by the Secretary-General of UNCTAD Introduction 1. This report provides an overview of independent evaluation activities`,
+      title: `S/AGENDA/6231 - UNFICYP TCC`,
+      description: `United Nations S/Agenda/6231 Security Council Distr.: General 7 December 2009 Original: English 09-63619 (E) 071209 *0963619* Provisional agenda for the 6231st (closed) meeting of the Security Council To be held in private on Monday, 7 December 2009, at 10.10 a.m. 1. Adoption of the agenda. 2. Meeting of the Security Council with the troop-contribu`,
       dataSource: "ODS",
-      url: "https://documents.un.org/symbol-explorer?s=TD/B/WP/330_1724067920850"
-    }
+      url: "https://documents.un.org/symbol-explorer?s=S/AGENDA/6231&i=S/AGENDA/6231_5724545"
+    },
+    {
+      title: `S/AGENDA/7223 - AGENDA - TCC UNFICYP`,
+      description: `United Nations S/Agenda/7223 Security Council Distr.: General 23 July 2014 Original: English 14-58155 (E) 230714 *1458155* Provisional agenda for the 7223rd (closed) meeting of the Security Council To be held in private on Wednesday, 23 July 2014, at 10 a.m. 1. Adoption of the agenda. 2. Meeting of the Security Council with the troop- and pol`,
+      dataSource: "ODS",
+      url: "https://documents.un.org/symbol-explorer?s=S/AGENDA/7223&i=S/AGENDA/7223_5092955"
+    },
+    
   ];
 
   // Simulate API data
